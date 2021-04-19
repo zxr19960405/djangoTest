855500dde95dadb2ed7b4b013e71acecbe428731
zhaoxinrong <1473233993@qq.com>
2021-04-19 18:30:39 +0800

    删除secretkey

--- djangoTest/settings.py
+++ djangoTest/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--2fch#zk$lcd$y=9r1a1r$*ps-*tz28^p4)zrilqqp#wcasn^0'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
